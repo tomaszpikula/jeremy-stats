f9880b928ff741259034adb94d41613b62eb9acf
Tomasz Pikula <tomasz.pikula.2@gmail.com>
2024-05-23 21:26:24 +0200

    Initial Commit

---
+++ Jeremy Stats/Jeremy_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jeremy_StatsApp.swift
+//  Jeremy Stats
+//
+//  Created by Tomasz Pikula on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Jeremy_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
