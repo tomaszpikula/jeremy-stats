04db35282d8376ac990b222fce26a52f3b36c366
Tomasz Pikula <tomasz.pikula.2@gmail.com>
2024-05-27 06:30:45 +0200

    add structs for Player Info View

--- Jeremy Stats/ContentView.swift
+++ Jeremy Stats/ContentView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
     @State var players = [Player]()
+    @State var jeremy = Player()
+    @State var jeremyIndex = 0
     var dataService = DataService()
+    
     
     var body: some View {
         VStack {
             Text(String(players.count))
+            ForEach (players) {player in
+                Text (String(player.jersey ?? 000 ))
+            }
         }
         .padding()
         .onAppear(perform: {
             Task {
                 await players = dataService.playersByTeam()
-                print(players)            }
+                jeremyIndex = players.firstIndex(where: {$0.id == 20002881}) ?? 0
+                jeremy = players[jeremyIndex]
+                print(jeremy)     
+            }
         })
     }
 }
--- Jeremy Stats/PlayerByTeam.swift
+++ Jeremy Stats/PlayerByTeam.swift
@@ -9,10 +9,44 @@ import Foundation
 
 struct Player: Identifiable, Decodable {
     var id: Int?
+    var jersey: Int?
+    var salary: Int?
     
     enum CodingKeys: String, CodingKey {
         
         case id = "PlayerID"
+        case jersey = "Jersey"
+        case salary = "Salary"
         
     }
+}
+
+struct PlayerStats: Identifiable, Decodable {
+    var id: Int?
+    var games: Int?
+    var points: Double?
+    var rebounds: Double?
+    var assists: Double?
+    var steals: Double?
+    var blockedShots: Double?
+    var fieldGoalsPercentage: Double?
+    var threePointersPercentage: Double?
+    var freeThrowsPercentage: Double?
+    
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id = "PlayerID"
+        case games = "Games"
+        case points = "Points"
+        case rebounds = "Rebounds"
+        case assists = "Assists"
+        case steals = "Steals"
+        case blockedShots = "BlockedShots"
+        case fieldGoalsPercentage = "FieldGoalsPercentage"
+        case threePointersPercentage = "ThreePointersPercentage"
+        case freeThrowsPercentage = "FreeThrowsPercentage"
+        
+    }
+    
 }
